d3a994e24ab80921c542779500b00c772ce381cc
oleg <opa@exante.eu>
2021-12-08 17:08:05 +0300

    Skeleton.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,73 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    async getMain(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Get()
+    async getDating(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Post()
+    async postDating(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Get()
+    async getLogin(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Post()
+    async postLogin(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Get()
+    async getOptions(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Post()
+    async postOptions(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Get()
+    async profile(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Get()
+    async register(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Post()
+    async postRegister(): Promise<string> {
+        // TODO -
+        return;
+    }
+
+    @Get()
+    async trade(): Promise<string> {
+        // TODO -
+        return;
+    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,31 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    async getDating(): Promise<void> {
+        // TODO -
+    }
+
+    async saveDating(): Promise<void> {
+        // TODO -
+    }
+
+    async login(): Promise<void> {
+        // TODO -
+    }
+
+    async getOptions(): Promise<void> {
+        // TODO -
+    }
+
+    async saveOptions(): Promise<void> {
+        // TODO -
+    }
+
+    async getProfile(): Promise<void> {
+        // TODO -
+    }
+
+    async register(): Promise<void> {
+        // TODO -
+    }
 }
